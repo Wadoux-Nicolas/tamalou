4185190d99e8f1b2069bc0c969a8218e26af5693
lauriemorin <laurie20010701@gmail.com>
2024-06-25 15:43:25 +0200

    add chakra ui

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,10 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import * as React from 'react'
+import {ChakraProvider, extendTheme} from '@chakra-ui/react'
+import * as ReactDOM from 'react-dom/client'
 import App from "./App.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const colors = {
+    blue: {
+        light: "#97CAD3",
+        main: "#13869A",
+    },
+    white: "#FFFFFF",
+    alert: "#F74C48",
+    black: "#000000"
+}
+
+
+const theme = extendTheme({ colors })
+
+const rootElement = document.getElementById('root')
+ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+        <ChakraProvider theme={theme}>
+            <App />
+        </ChakraProvider>
+    </React.StrictMode>,
+)
