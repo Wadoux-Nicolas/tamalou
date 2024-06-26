008f03a6edae569b88067320bfe76f7a7306c93e
chiekhou <chiekhou.traore@gmail.com>
2024-06-26 12:35:27 +0200

    récupération de tous les messages du patients

--- back/main.py
+++ back/main.py
@@ -11,18 +11,26 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-
 class InputRequest(BaseModel):
     input: str
-
+    
+messages = []
 
 @app.post("/summarize")
 def read_item(input_request: InputRequest):
     output = generate_summary(input_request.input)
-    return {"input": input_request.input, "output": output}
+    return {"input": input_request.input, "output": output,}
 
 
 @app.post("/message")
 def read_item(input_request: InputRequest):
     output = classification(input_request.input)
+    messages.append({"message":input_request.input , "catégorie": output['labels']})
+    print("Messages:", messages)
     return {"input": input_request.input, "output": output}
+
+
+@app.get("/messages")
+def read_item():
+    return messages
+
