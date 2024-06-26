6ab4bd94636909d7030d430031cdd13cb3121455
Nicolas W <nwadoux@myges.fr>
2024-06-26 10:12:07 +0200

    add patient card

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,21 @@
 import "./App.css";
 import Header from "./components/Header.tsx";
+import PatientCard from "./components/PatientCard.tsx";
+import {Box} from "@chakra-ui/react";
+import {PatientMauriceDupont} from "./mocks/patient.ts";
 
 function App() {
   return (
     <>
         <Header/>
+        <Box
+            maxW='800px'
+            p='8'
+        >
+            <PatientCard
+                patient={PatientMauriceDupont}
+            />
+        </Box>
     </>
   );
 }
---
+++ front/src/models/patient.ts
@@ -0,0 +1,10 @@
+export interface Patient {
+    id: number;
+    firstName: string;
+    lastName: string;
+    age: number;
+    height: number;
+    weight: number;
+    nationality: string;
+    pathologiesSummary: string;
+}
